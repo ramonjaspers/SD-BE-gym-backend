c4df1439e34b0bb9f4ac0a85f40806c176a55f07
ramonjaspers <ramonj360@gmail.com>
2022-04-26 22:34:59 +0200

    refactor services

--- src/main/java/gym/gymbackend/service/FacilityService.java
+++ src/main/java/gym/gymbackend/service/FacilityService.java
@@ -6,15 +6,53 @@ import gym.gymbackend.model.Facility;
 import java.util.List;
 
 public interface FacilityService {
+    /**
+     * get facilities
+     *
+     * @return {@link List}
+     * @see List
+     * @see Facility
+     */
     List<Facility> getFacilities();
 
+    /**
+     * get facility by id
+     *
+     * @param id id
+     * @return {@link Facility}
+     * @see Facility
+     */
     Facility getFacility(Long id);
 
+    /**
+     * get facilities by membership
+     *
+     * @param membership membership
+     * @return {@link List}
+     * @see List
+     * @see Facility
+     */
     List<Facility> getFacilitiesByMembership(String membership);
 
+    /**
+     * create facility
+     *
+     * @param facilityDto facilityDto
+     */
     void createFacility(FacilityDto facilityDto);
 
+    /**
+     * delete facility by id
+     *
+     * @param id id
+     */
     void deleteFacility(Long id);
 
+    /**
+     * update facility by id
+     *
+     * @param id          id
+     * @param facilityDto facilityDto
+     */
     void updateFacility(Long id, FacilityDto facilityDto);
 }
